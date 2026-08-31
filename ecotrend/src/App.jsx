260ee086b2e91c050172d56db678c8858012e002
matheusdonasv <matheusdonasv@gmail.com>
2026-08-31 18:40:47 -0300

    adiciona	mensagem de sucesso e erro	no checkout

--- ecotrend/src/App.jsx
+++ ecotrend/src/App.jsx
@@ -22,3 +22,36 @@ function App() {
 }
 
 export default App;
+
+import { useState } from 'react';
+import { ProductFilters } from './components/ProductFilters';
+import { ProductList } from './components/ProductList';
+import { Cart } from './components/Cart';
+import { Checkout } from './components/Checkout';
+
+function App() {
+  const [category, setCategory] = useState('todos');
+  const [maxPrice, setMaxPrice] = useState(500);
+
+  return (
+    <div className="app">
+      <ProductFilters
+        category={category}
+        setCategory={setCategory}
+        maxPrice={maxPrice}
+        setMaxPrice={setMaxPrice}
+      />
+
+      <ProductList
+        category={category}
+        maxPrice={maxPrice}
+      />
+
+      <Cart />
+
+      <Checkout />
+    </div>
+  );
+}
+
+export default App;
