562af7ec773b2849091073ca08f848536a545bc3
Enzo Cassiano <enzocm786@gmail.com>
2026-08-31 18:21:58 -0300

    integra filtros no app

--- ecotrend/src/App.jsx
+++ ecotrend/src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+import { ProductFilters } from './components/ProductFilters';
 import { ProductList } from './components/ProductList';
 import { Cart } from './components/Cart';
 
 function App() {
+  const [category, setCategory] = useState('todos');
+  const [maxPrice, setMaxPrice] = useState(500);
+
   return (
     <div className="app">
-      <ProductList />
+      <ProductFilters
+        category={category}
+        setCategory={setCategory}
+        maxPrice={maxPrice}
+        setMaxPrice={setMaxPrice}
+      />
+      <ProductList category={category} maxPrice={maxPrice} />
       <Cart />
     </div>
   );
